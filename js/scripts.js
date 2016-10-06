b7206c5d806285a5ea7185f623e7e288d002b9c7
hayleygay <hgay1@vcd-ch204-17.eastern.ewu.edu>
2016-10-06 14:29:26 -0700

    Version 0

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Test scripts.js
+console.log('OK USA!');
+
+
+//Test jQuery
+// $('img').hide();
+
+$('img').imageLens({
+    imageSrc: '/img/watch-zoom.jpg',
+    lensSize: 400,
+    borderColor: '#FFFF31'
+});
